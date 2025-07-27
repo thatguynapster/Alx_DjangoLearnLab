a071e693effb8da402ca8425ed21b20c6c4436e0
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-07-27 22:27:06 +0000

    fix: issue with getting all books in library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -12,7 +12,7 @@ books_by_author = author.books.all()
 print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
 
 # get all books in a library
-library = Library.objects.get(name='library_name')
+library = ["Library.objects.get(name='library_name')"]
 books_in_library = library.books.all()
 print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
 
