5efcb86317bada246d628b005b21088ae8ad3eb7
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-07-27 23:46:53 +0000

    fix:checker issues

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Library, Book
 
 def list_books(request):
     books = Book.objects.all()
