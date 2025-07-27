82e7bb2954bc851640e14cbc30256ce80f7864b8
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-07-27 23:59:29 +0000

    user auth assignment

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library, Book
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
 
 def list_books(request):
     books = Book.objects.all()
@@ -11,3 +15,14 @@ class libraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html' 
     context_object_name = 'library'
+    
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log in after registration
+            return redirect('list_books')  # redirect to some page after login
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
