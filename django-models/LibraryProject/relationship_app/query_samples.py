13202afdbc80f81d2e4be2980327a46092858583
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-07-27 22:34:37 +0000

    fix: issue with getting all books in library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,15 +7,22 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 # get all books by an author
-author = Author.objects.get(name='Chinua Achebe')
-books_by_author = author.books.all()
-print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
+author_name = input("Enter author name: ")
+try:
+    author = Author.objects.get(name=author_name)
+    books_by_author = author.books.all()
+    print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
+except Author.DoesNotExist:
+    print(f"No author found with the name {author_name}")
 
 # get all books in a library
-library_name = "Central Library"
-library = Library.objects.get(name=library_name)
-books_in_library = library.books.all()
-print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
+library_name = input("Enter library name: ")
+try:
+    library = Library.objects.get(name=library_name)
+    books_in_library = library.books.all()
+    print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
+except Library.DoesNotExist:
+    print(f"No library found with the name {library_name}")
 
 # get librarian for a library
 librarian = library.librarian
