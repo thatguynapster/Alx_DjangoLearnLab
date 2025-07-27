1341b4bbf7a45317494313e3c506d6288b6e1e0c
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-07-27 23:43:42 +0000

    fix:checker issues

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import list_books, libraryDetailView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', libraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from .relationship_app.views import list_books, libraryDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
