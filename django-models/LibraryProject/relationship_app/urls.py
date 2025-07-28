f0493926fe025c763060a7f7a83e7d0125bd31f8
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-07-28 00:32:04 +0000

    fix: issue with task 1 checkers

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
-from .views import libraryDetailView, list_books
+from .views import  list_books, LibraryDetailView
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', libraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     
     # # Auth URLs
     # path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,7 +13,7 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books}) 
 
 
-class libraryDetailView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html' 
     context_object_name = 'library'
