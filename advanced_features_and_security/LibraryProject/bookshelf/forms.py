f2964b8b0dc81966443dc0a590f6e5fd7f9240d0
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-08-03 21:56:11 +0000

    fixed issue with bookshelf form

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,11 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
+        
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+    
