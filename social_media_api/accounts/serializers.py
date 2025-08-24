3ece6e2407f0670cbc4f9cc9d43f5688134ff73c
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-08-24 22:06:02 +0000

    fixed issue with checker

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -20,7 +20,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["username", "email", "password"]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
