20382598ce37bcdefc80235024463b6c4e6c7f72
Andrew Osei-Wusu <andrewosei94@gmail.com>
2025-08-25 07:51:03 +0000

    fix issues with checker

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,4 +1,11 @@
-from .views import UserViewSet, RegisterView, LoginView, ProfileView
+from .views import (
+    UserViewSet,
+    RegisterView,
+    LoginView,
+    ProfileView,
+    follow_user,
+    unfollow_user,
+)
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
@@ -10,5 +17,7 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
+    path("follow/<int:user_id>/", follow_user, name="follow_user"),
+    path("unfollow/<int:user_id>/", unfollow_user, name="unfollow_user"),
     path("", include(router.urls)),
 ]
